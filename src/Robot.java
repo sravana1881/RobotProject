f33c222b463daecd9254d5d0e9b2955390cf2d25
sravana karne <sandhyajava42@gmail.com>
2024-11-14 12:55:32 -0600

    commiting changes robot project.

--- src/Robot.java
+++ src/Robot.java
@@ -4,22 +4,26 @@ public class Robot {
     String name="Robo";
     String color="Metalic Gray";
     int weight=200;
-
+//Move
     public void move() {
         speed = 5;
         System.out.println(name + " is moving at speed " + speed + " mph.");
     }
+    //Stop
     public void stop() {
         speed = 0;
         System.out.println(name + " has stopped.");
     }
+    //Charge Battery
     public void chargeBattery() {
         batteryLevel = 100;
         System.out.println(name + "'s battery is fully charged.");
     }
+    //Check Battery Level
     public void checkBatteryLevel() {
         System.out.println(name + "'s current battery level is " + batteryLevel + "%.");
     }
+    //Display Info
     public void displayInfo(){
       System.out.println( name + "'s color is "+ color +" weight is " + weight + "lb.");
     }
